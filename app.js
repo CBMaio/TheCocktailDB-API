f69113f1dbd52ce121e1978fb7cae9a6fec1ffc9
caroMaio <carolinabelenmaio@gmail.com>
2020-12-21 13:11:21 -0300

    [DELETE] Instructions btn, add details in card

--- app.js
+++ app.js
@@ -17,19 +17,22 @@ const createNode = ({
   strAlcoholic,
   idDrink,
 }) => {
+
   const node = `
     <div class="col-md-4 col-12" id="${idDrink}">
-      <div class="card mt-5 ml-md-3">
+      <div class="card card-style mt-5 ml-md-3">
         <img src="${strDrinkThumb}" class="drink-img"/>
         <div class="card-body">
 
           <h5 class="card-title"> ${strDrink} </h5>
           <p class="card-text">Category: ${strCategory}.</p>
           <p> Alcoholic: ${strAlcoholic} </p>
-          <ul id="ingredients-list-${idDrink}"></ul>
+          
+          <p><b>Ingredients:</b> ${selectDrink(idDrink).join(', ')}</p>
           <p id="text-${idDrink}"></p>
-          <button onclick="selectDrink(${idDrink})" class="btn btn-block btn-secondary">Ver ingredientes</button>
-          <button onclick="showInstructions(${idDrink})" class="btn btn-block btn-secondary">Ver instrucciones</button>
+          <div>
+            <button onclick="showInstructions(${idDrink})" class="w-100  btn btn-block btn-secondary">Instructions</button>
+          </div>
         </div>
       </div>
     </div>
@@ -47,40 +50,40 @@ const del = (id) => {
 const selectDrink = (idDrink) => {
   let drink = drinks.find((drink) => drink.idDrink === `${idDrink}`);
   let i = 1;
+  let ingredients = [];
   while (drink[`strIngredient${i}`] !== null) {
-    const description = drink[`strIngredient${i}`];
-    const listItem = `
-      <li>${description}</li>
-    `;
-    document
-      .getElementById(`ingredients-list-${idDrink}`)
-      .insertAdjacentHTML("beforeend", listItem);
+    ingredients.push(drink[`strIngredient${i}`]);
     i++;
   }
+
+  return ingredients;
 };
 
 const showInstructions = (idDrink) => {
   let drink = drinks.find((drink) => drink.idDrink === `${idDrink}`);
-  document.getElementById(`text-${idDrink}`).innerHTML = drink.strInstructions;
-}
+  document.getElementById(`text-${idDrink}`).innerHTML = `
+    <div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Instructions</h4>
+    <p>${drink.strInstructions}</p>
+    <hr>
+    </div>
+  `
+};
 
-//iteración de array drinks, muestra una card por cada drink 
+//iteración de array drinks, muestra una card por cada drink
 const iterateDrinks = (drinks) => {
   drinks.map((drink) => {
     createNode(drink);
   });
 };
 
-
 //toma el valor del input search y genera un nuevo array con los tragos q contengan ese ingrediente
 const searchDrink = async () => {
-  drinks.map(drink => del(drink.idDrink));
-  const  {value} = document.getElementById('ingredient-name');
-  const urlSearch = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`
-  drinks = await fetchData(urlSearch)
-  console.log(drinks)
-  iterateDrinks(drinks)
-
+  drinks.map((drink) => del(drink.idDrink));
+  const { value } = document.getElementById("ingredient-name");
+  const urlSearch = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
+  drinks = await fetchData(urlSearch);
+  iterateDrinks(drinks);
 };
 
 const start = async () => {
